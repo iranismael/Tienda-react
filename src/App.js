e405015c34a9fa30bca7d4f834113f9c756518aa
Ismael <iranisma10@gmail.com>
2022-08-09 17:41:39 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import styled from "styled-components";
+import { NavLink, Route, Routes } from "react-router-dom";
+import Tienda from "./componentes/Tienda";
+import Blog from "./componentes/Blog";
+import Inicio from "./componentes/Inicio";
+import Error404 from "./componentes/Error404";
+import Carrito from "./componentes/Carrito";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  const productos = [
+    {id: 1, nombre:'producto1'},
+    {id: 2, nombre:'producto2'},
+    {id: 3, nombre:'producto3'},
+    {id: 4, nombre:'producto4'},
+];
+const [carrito, cambiarCarrito] = useState([]);
+
+const agregarProductoAlCarrito = (idProducto, nombre ) =>{
+
+    //Si el carrito no tiene elementos entonces agregamos uno
+  if(carrito.length === 0){
+    cambiarCarrito([{id:idProducto, nombre:nombre, cantidad:1}]);
+  }else{
+    //De otro forma tenemos que revisar que el carrito no tenga ya el producto agregado
+    //Si ya lo tiene actualizamos el valor
+    //Si no entonces lo agregamos
+    //Para poder editar el arreglo tenemos que clonarlo
+    const nuevoCarrito = [...carrito];
+
+    //Comprobar si el carrito ya tiene el ID del producto
+
+    const YaEstaEnCarrito = nuevoCarrito.filter( ( productoDeCarrito )=>{
+      return productoDeCarrito.id === idProducto
+    }).length > 0;
+    
+    //Si ya esta en producto entonces lo actualizamos
+    if(YaEstaEnCarrito){
+      //Para ello  tenemos que buscarlo, obtener su posicion en el arreglo
+      //y en base a su posicion lo actualizamos
+
+      nuevoCarrito.forEach((productoDeCarrito, index)=> {
+
+        if(productoDeCarrito.id === idProducto){
+          const cantidad = nuevoCarrito[index].cantidad;
+          nuevoCarrito[index] ={
+            id:idProducto,
+            nombre:nombre,
+            cantidad:cantidad +1
+          }
+        }
+
+      });
+      //De otra forma entonces  agregamos el producto al arreglo
+    }else{
+      nuevoCarrito.push(
+        {
+          id:idProducto,
+          nombre: nombre,
+          cantidad: 1
+        }
+      );
+    }
+    //Actualizar el carrito
+    cambiarCarrito(nuevoCarrito);
+
+  }
 }
 
-export default App;
+  return (
+    <Contenedor>
+      <Menu>
+        <NavLink to="/">Inicio</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="/tienda">Tienda</NavLink>
+      </Menu>
+
+      <main>
+        <Routes>
+
+          <Route path="*" element={<Error404/> } />
+
+          <Route path="/" element={
+            <Inicio productos={productos}/> 
+            } 
+          />
+          <Route path="/blog" element={
+            <Blog productos={productos} /> 
+            } 
+          />
+          <Route path="/tienda" element={
+            <Tienda 
+              productos={productos}
+              agregarProductoAlCarrito = {agregarProductoAlCarrito} 
+            /> 
+            } 
+          />
+
+          
+        </Routes>
+
+      </main>
+
+      <aside>
+            <Carrito carrito={carrito}/>
+      </aside>
+    </Contenedor>
+
+  )
+}
+
+const Contenedor = styled.div`
+	max-width: 1000px;
+	padding: 40px;
+	width: 90%;
+	display: grid;
+	gap: 20px;
+	grid-template-columns: 2fr 1fr;
+	background: #fff;
+	margin: 40px 0;
+	border-radius: 10px;
+	box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+
+const Menu = styled.nav`
+	width: 100%;
+	text-align: center;
+	background: #092c4c;
+	grid-column: span 2;
+	border-radius: 3px;
+
+	a {
+		color: #fff;
+		display: inline-block;
+		padding: 15px 20px;
+	}
+
+	a:hover {
+		background: #1d85e8;
+		text-decoration: none;
+	}
+`;
+
+export default App
---
+++ src/componentes/Carrito.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Carrito = ({carrito}) => {
+    
+
+  return (
+    <div>Carrito
+        { carrito.length > 0 ?
+            carrito.map((producto, index)=>{
+                return(
+                   <Producto key={index}>
+                       <NombreProducto>
+                            {producto.nombre}
+                        </NombreProducto> 
+                        Cantidad {producto.cantidad}
+                   </Producto> 
+                );
+            })
+           
+        :
+            <p>Aún no haz agregado productos al carrito</p>
+        }
+    </div>
+  )
+}
+
+const Producto = styled.div`
+    padding: 10px;
+    border-bottom: 1px solid #ebebf3;
+    font-size: 14px
+`;
+
+const NombreProducto = styled.p`
+    font-weight: bold;
+    font-size: 16px;
+    color: #000;
+`;
+
+export default Carrito
